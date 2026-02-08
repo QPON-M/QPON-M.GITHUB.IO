f7a8b1d1f118c8c44405bcfeb70b0e9037169468
QPON-M <meirhadad77@gmail.com>
2026-02-08 19:48:14 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,29 @@
 const CACHE_NAME = 'qpon-m-v1';
-const ASSETS = [
-    './',
-    './index.html',
-    './manifest.json',
-    './icon.jpg',
-    'https://cdn.tailwindcss.com',
-    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
-    'https://fonts.googleapis.com/css2?family=Heebo:wght@300;400;500;700;900&display=swap'
+const urlsToCache = [
+  './',
+  './index.html',
+  './icon.jpg',
+  './manifest.json',
+  'https://cdn.tailwindcss.com',
+  'https://fonts.googleapis.com/css2?family=Heebo:wght@300;400;500;700;900&display=swap',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
 ];
 
-self.addEventListener('install', (e) => {
-    e.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-    );
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener('fetch', (e) => {
-    e.respondWith(
-        caches.match(e.request).then((response) => response || fetch(e.request))
-    );
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
 });
